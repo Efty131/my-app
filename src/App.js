bb14c4c6edf963d99a1241689d8c0880836e8ae0
Efty <efty131331@gmail.com>
2022-03-26 11:42:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+const h1 = {
+  textAlign: "center",
+  fontSize: "3rem",
+  fontFamily: "sans-serif",
+  animation: "animate 5s linear infinite"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 style={h1}>Bismillah</h1>
     </div>
   );
 }
