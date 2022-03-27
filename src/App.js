0e590460b51a8dc754d977975f90a5fa7a569325
Efty <efty131331@gmail.com>
2022-03-27 12:41:03 +0600

    hdddgdh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 function App() {
   const apiKey = "9d09e907aa6c091f62b4c7ad1f2b6387";
   const [inputCity, setInputCity] = useState("");
-  const [data, setData] = useState({});
+  const [city, setCity] = useState("Bhaluka");
+  const [data, setData] = useState([{}]);
   const [imgIndex, setImgIndex] = useState(1);
 
   const getWeatherDetails = (cityName) => {
@@ -20,15 +21,16 @@ function App() {
     axios
       .get(apiURL)
       .then((res) => {
-        console.log("response", res.data);
-
+        setCity(cityName);
         setData(res.data);
+        
       })
       .catch(() => {
         alert("No data found");
       });
   };
   const handleInputCity = (e) => {
+    
     setInputCity(e.target.value);
   };
 
@@ -38,30 +40,35 @@ function App() {
     getWeatherDetails(inputCity);
   };
 
+  
   let temp = (data?.main?.temp - 273.15).toFixed(0);
   let temp_min = (data?.main?.temp_min - 273.15).toFixed(2);
   let temp_max = (data?.main?.temp_max - 273.15).toFixed(2);
-  // let desc = (data?.weather?.main);
-  // console.log(data.weather)
 
+  let wind = (data?.wind?.speed);
+
+  console.log(data.id)
+  
+
+   let humidity = (data?.main?.humidity);
   return (
     //         starts from here
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-4">
-          <div class="card bg-dark text-center text-white border-0">
+          <div className="card bg-dark text-center text-white border-0">
             <img
               // src="https://source.unsplash.com/600x900/?nature,water"
               src={"https://picsum.photos/200/300?random=" + imgIndex}
-              class="card-img"
+              className="card-img"
               alt="..."
             />
-            <div class="card-img-overlay">
+            <div className="card-img-overlay">
               <form>
-                <div class="input-group mb-4 w-75 mx-auto">
+                <div className="input-group mb-4 w-75 mx-auto">
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     placeholder="City Name"
                     aria-label="Recipient's username"
                     aria-describedby="basic-addon2"
@@ -69,7 +76,7 @@ function App() {
                   />
                   <button
                     tyep="button"
-                    class="input-group-text"
+                    className="input-group-text"
                     id="basic-addon2"
                     onClick={handleSearch}
                   >
@@ -78,15 +85,17 @@ function App() {
                 </div>
               </form>
               <div className="bg-dark bg-opacity-50 py-3">
-                <h2 class="card-title">{data.name}</h2>
-                <p class="card-text lead">{new Date().toLocaleDateString()}</p>
+                <h2 className="card-title">{city}</h2>
+                <p className="card-text lead">{new Date().toLocaleDateString()}</p>
                 <hr />
                 <i className="fas fa-cloud w-75"></i>
-                <h2>{temp} &deg;C</h2>
+                <h2>{temp}&deg;C</h2>
                 <p className="lead fw-bolder mb-0">cloud</p>
                 <p className="lead">
                   {temp_min}&deg;C | {temp_max}&deg;C
                 </p>
+                <p>Humidity: {humidity}</p>
+                <p>Wind Speed: {wind}</p>
               </div>
             </div>
           </div>
